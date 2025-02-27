2722f42501f9897fc63a6ab362193720b3718598
Nao D <decima.nao@gmail.com>
2025-02-27 17:11:45 -0300

    Initial commit

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req,res){
+  let ip = req.ip;
+  let lang = req.language;
+  let sw = req.software;
+  res.json( {ipaddress: ip, language: lang, software: sw} )
+})
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
